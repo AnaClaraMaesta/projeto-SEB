a044ae6f90cb44c36a3a863cb5b15b7dac661f58
maesta <maesta.zrck@gmail>
2026-08-21 21:11:42 -0300

    Criando botoes legais

--- seb/resources/views/layouts/homePage.blade.php
+++ seb/resources/views/layouts/homePage.blade.php
@@ -2,48 +2,35 @@
 
 @section('dashboard')
 
-<header class="bg-[#e6eaf0] dark:bg-[#464a4f] shadow-md">
-    <div class="relative inline-block text-left">
+<header class="bg-[#3D2F2F] text-white dark:bg-[#464a4f] shadow-md">
+    <div  x-data="{ open: false}" class="relative inline-block text-left">
 
-        <button id="dropdownbutton" class="btn cursor-pointer hover:bg-[#3D2F2F] p-2.5 m-3">
-            Alunos
+        <button @click="open = !open" class="btn cursor-pointer hover:bg-[#3D2F2F] p-2.5">
+            ALUNOS
             <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
             </svg>
         </button>
 
-        <button id="dropdownbutton" class="btn cursor-pointer hover:bg-[#3D2F2F] p-2.5 m-3">
-            Livros
+        <button class="btn cursor-pointer hover:bg-[#3D2F2F] p-2.5">
+            LIVROS  
             <svg class="w-4 h-4 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
             </svg>
         </button>
 
-        
-        <!-- x-data gerencia o estado (aberto ou fechado) do item -->
-        <div x-data="{ aberto: false }" class="border border-red-500 rounded mb-2">
-                <!-- Botão que inverte o estado ao clicar -->
-            <button @click="aberto = !aberto" class="w-full text-left p-4  flex justify-between">
-                <span>Título do Accordion</span>
-                <span x-text="aberto ? '-' : '+'"></span>
-            </button>
-
-            <!-- Conteúdo que só aparece se 'aberto' for true -->
-          <div x-show="aberto" x-transition class="p-4 border-t border-yellow-500">
-                Este conteúdo agora abre e fecha perfeitamente!
-          </div>
-        </div>
-
-        
-        
-        
-        <div class="hidden absolute">
-            <div class="py-2">
-                <a href="">Estoque</a> <!-- dentro de estoque vai ter a opção de cadastrar e excluir além de emitir relatórios também -->
-                <a href="">Emprestar</a> 
+        <div x-show="open" x-transition class=" border-t-2 border-yellow-500">
+            <div class="">
+                <button class="btn cursor-pointer hover:bg-[#2F3D3D] w-full dark:hover:bg-[#3D2F2F]">
+                    CADASTRAR
+                </button> <!-- dentro de estoque vai ter a opção de cadastrar e excluir além de emitir relatórios também -->
+            </div>
+            <div class="">
+                <button class="btn cursor-pointer hover:bg-[#2F3D3D] w-full dark:hover:bg-[#3D2F2F]">
+                    EMPRESTAR
+                </button>
             </div>
         </div>
-        
 
     </div>
 </header>
