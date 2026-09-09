df3c8bc2bb61e3312d961224f7be123f7a171268
maesta <maesta.zrck@gmail>
2026-09-08 21:21:00 -0300

    Editando rotas

--- seb/routes/web.php
+++ seb/routes/web.php
@@ -3,18 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 
-Route::get('/', function () {
-    return view('layouts.login');
-});
-
 Route::get('/dashboard', function(){
     return view('layouts.homePage');
 });
 
+Route::get('/login',[LoginController::class, 'create']) -> name('login');
 
+Route::post('/login',[LoginController::class, 'store']) -> name('login.store');
 
 Route::get('/criar-usuario', [userController::class, 'create']) -> name('usuarios.create');
 /* Route::get('URL', [nome do Controller::class, 'nome da função do controller']) -> definir nome para identificar como rota name('usuarios.cadastro'); */
-
-Route::get('/login',[LoginController::class, 'create']) -> name('login');
-Route::post('/login',[LoginController::class, 'store']) -> name('login.store');
